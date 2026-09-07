c2afdd54dce1446dc4694ca9101884f08fad2dc8
André Luiz Nogueira <kayobi.bc@gmail.com>
2026-09-07 16:37:09 -0300

    versao

--- salad_survivors/flutter_bootstrap.js
+++ salad_survivors/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "376421791" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "149460630" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
